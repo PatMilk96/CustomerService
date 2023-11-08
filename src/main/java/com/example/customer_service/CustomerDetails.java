278217110df1ed3551a790785912f4dea56a46e3
Patryk Milkiewicz <G00397265@atu.ie>
2023-11-08 09:49:51 +0000

    created a method in the CustomerService class to add a customer and retrieve a customer by their Id

--- src/main/java/com/example/customer_service/CustomerDetails.java
+++ src/main/java/com/example/customer_service/CustomerDetails.java
@@ -1,4 +1,18 @@
 package com.example.customer_service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CustomerDetails {
+    private int customerId;
+    private String name;
+    private int age;
+    private String email;
 }
--- src/main/java/com/example/customer_service/CustomerService.java
+++ src/main/java/com/example/customer_service/CustomerService.java
@@ -2,6 +2,21 @@ package com.example.customer_service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class CustomerService {
+    public ArrayList<CustomerDetails> orders = new ArrayList<>();
+
+    public void createCustomer(CustomerDetails customerDetails) {
+        orders.add(customerDetails);
+    }
+
+    public CustomerDetails findById(int id) {
+        for(CustomerDetails order : orders){
+            if(order.getCustomerId() == id) {
+                return order;
+            }
+        }
+        return null;
 }
